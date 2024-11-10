1181765a7dbf7ae829d457c2efb1c42b196b337a
letalandroid <carlossoncra@gmail.com>
2024-11-10 09:46:54 -0500

    Reformat styles to "Tenemos lo que estás buscando" section

--- src/components/ProductCard.jsx
+++ src/components/ProductCard.jsx
@@ -3,11 +3,13 @@ import React from 'react';
 
 const ProductCard = ({ image, title }) => {
   return (
-    <div className="card text-center border-0 shadow-sm mb-4" style={{ borderRadius: '10px' }}>
+    <div className="card position-relative text-center border-0 shadow-sm mb-4" style={{ borderRadius: '10px' }}>
       <div className="card-header bg-danger text-white">{title}</div>
-      <img src={image} alt={title} className="card-img-top" style={{ borderRadius: '0 0 10px 10px' }} />
-      <div className="card-body">
-        <button className="btn btn-outline-light bg-danger w-100">Me interesa</button>
+      <img src={image} alt={title} className="w-100 h-100" style={{ borderRadius: '0 0 10px 10px' }} />
+      <div className="position-absolute bottom-0 w-100 p-3">
+        <button className="btn btn-outline-light bg-white w-100">
+          <span className='text-danger'>Me interesa</span>
+        </button>
       </div>
     </div>
   );
